08913bbcc4a489eedb886190c94c2299d52093dd
zhou <2307771819@qq.com>
2025-12-08 21:51:08 +0800

    chore: viteConfig配置；env.* 环境变量配置。

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
+import { defineConfig, loadEnv } from 'vite'
+import viteBaseConfig from './vite.base.config'
+import viteDevConfig from './vite.dev.config'
+const envResolve = {
+    serve: () => {
+        console.log('开发环境')
+        return Object.assign({}, viteBaseConfig, viteDevConfig)
+    },
+    build: () => {
+        console.log('生产环境')
+        return Object.assign({}, viteBaseConfig, viteDevConfig)
+    },
+}
 // https://vite.dev/config/
-export default defineConfig({
-    plugins: [vue()],
+export default defineConfig(({ command, mode }) => {
+    const env = loadEnv(mode, process.cwd(), 'VITE_')
+    console.log('evn', env)
+    console.log('mode', mode)
+    return envResolve[command]()
 })
